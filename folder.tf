622cdd60841ced23a7c0713d8f7991f040907f95
Arslan Saeed <arslan@hashicorp.com>
2019-01-31 05:29:07 -0500

    remove tags.

--- folder.tf
+++ folder.tf
@@ -3,6 +3,6 @@ resource "vsphere_folder" "folder" {
   path              = "${var.prefix}"
   type              = "vm"
   datacenter_id     = "${data.vsphere_datacenter.dc.id}"
-  tags              = ["${vsphere_tag.tagProd.id}"]
-  custom_attributes = "${map(vsphere_custom_attribute.att_demo.id, "${var.attDemoValue}")}"
+//tags              = ["${vsphere_tag.tagProd.id}"]
+//custom_attributes = "${map(vsphere_custom_attribute.att_demo.id, "${var.attDemoValue}")}"
 }
