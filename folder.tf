fb397107edc8378ba22e03cf9b7baee9ad46703b
Arslan Saeed <arslan@hashicorp.com>
2019-01-31 04:58:36 -0500

    tagProd

--- folder.tf
+++ folder.tf
@@ -3,6 +3,6 @@ resource "vsphere_folder" "folder" {
   path              = "${var.prefix}${var.vmfolder}"
   type              = "vm"
   datacenter_id     = "${data.vsphere_datacenter.dc.id}"
-  tags              = ["${vsphere_tag.tag.id}"]
+  tags              = ["${vsphere_tag.tagProd.id}"]
   custom_attributes = "${map(vsphere_custom_attribute.att_demo.id, "${var.attDemoValue}")}"
 }
